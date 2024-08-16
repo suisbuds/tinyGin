2f7c7b6487c3c211585174bf196182dd6b3addb9
suisbuds <public321827@outlook.com>
2024-08-16 20:45:32 +0800

    实现路由分组控制,engine继承group所有属性

--- gee/gee.go
+++ gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,23 +10,50 @@ type HandleFunc func(*Context)
 
 // 映射路由表router, key为请求方法+请求路径，value为处理方法
 type Engine struct {
-	router *router
+	router       *router
+	*RouterGroup                // 继承RouterGroup的属性
+	groups       []*RouterGroup // 存储所有的group
+}
+
+// 路由组,实现分组添加路由
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandleFunc // 支持中间件
+	engine      *Engine      // 所有的group共享一个engine实例
+	parent      *RouterGroup // 支持嵌套
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandleFunc) {
-	engine.router.addRoute(method, pattern, handler)
+// 创建路由组
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		parent: g,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (engine *Engine) GET(pattern string, handler HandleFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (g *RouterGroup) addRoute(method string, pattern string, handler HandleFunc) {
+	pattern = g.prefix + pattern
+	log.Printf("Route %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandleFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (g *RouterGroup) GET(pattern string, handler HandleFunc) {
+	g.addRoute("GET", pattern, handler)
+}
+
+func (g *RouterGroup) POST(pattern string, handler HandleFunc) {
+	g.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
